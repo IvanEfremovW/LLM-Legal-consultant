3452441012034195bb51cd0b5505c325dc8a5243
IvanEfremovW <Ivan.EfremovWM@yandex.ru>
2025-09-29 21:29:02 +0300

    feat: add ChromaDB functional
    - Added function to create and get ChromaDB
    - Added default path of ChromaDB index "chroma_db" to .gitignore

--- src/ingestion.py
+++ src/ingestion.py
@@ -1,7 +1,12 @@
 import re
 from langchain_core.documents import Document
+from langchain_core.documents import Document
+from langchain_chroma import Chroma
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 
 from src.config import DATA_DIR
+from src.config import CHROMA_PATH, EMBEDDING_MODEL
+
 
 def load_documents() -> list[Document]:
     """
@@ -43,3 +48,23 @@ def load_documents() -> list[Document]:
             documents.append(document)
             
     return documents
+
+def get_chroma_vectorstore() -> Chroma:
+    """
+    Returns initialized ChromaDB.
+    
+    If index exist - returns existing index.
+    If not - create index and then return it.
+    
+    Returns:
+        Chroma: ChromaDB index.
+    """
+    
+    embeddings = HuggingFaceBgeEmbeddings(
+        model_name=EMBEDDING_MODEL
+    )
+    
+    return Chroma(
+        persist_directory=str(CHROMA_PATH),
+        embedding_function=embeddings
+    ) 
